af3d0ff6fcd586bf69aeb0f056fbfea172d6d346
vitalikreykjavik <serada.vitali@yandex.ru>
2020-10-25 22:10:22 +0300

    feat: add brackets task function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let bracketsCollection = {};
+  bracketsConfig.forEach(element => {
+    bracketsCollection[element[0]] = element[1];
+  });
+
+  let givenBrackets = str.split('');
+
+  let arrayOfBrackets = [];
+  //let bracketToCompare;
+  for (let i = 0; i < givenBrackets.length; i++) {
+    let bracketToCompare = arrayOfBrackets[arrayOfBrackets.length - 1];
+    if (bracketsCollection[bracketToCompare] == givenBrackets[i]) {
+      arrayOfBrackets.pop();
+    } else {
+      arrayOfBrackets.push(givenBrackets[i]);
+    };
+  };
+
+  if (arrayOfBrackets.length === 0) {
+    return true;
+  } else {
+    return false;
+  };
+};
